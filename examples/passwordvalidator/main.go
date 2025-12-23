4d84326458501af25255d4ac22489f18edc40400
DevTobias <github@tobiaskaerst.dev>
2025-12-23 17:26:30 +0100

    feat: fixed recovery code generation

--- examples/passwordvalidator/main.go
+++ examples/passwordvalidator/main.go
@@ -10,7 +10,7 @@ func main() {
 	const minEntropyBits = 60
 
 	fmt.Printf("Entropy: %f bits\n", strivia_password.GetPasswordEntropy("12345678"))
-	isValid := strivia_password.ValidatePasswordStrength("Simpsons1309!", minEntropyBits)
+	isValid := strivia_password.ValidatePasswordStrength("tad5YUj3Yh4RRuCt4HbesuXP71DxylQU", minEntropyBits)
 
 	fmt.Printf("Password is valid: %t\n", isValid)
 }
